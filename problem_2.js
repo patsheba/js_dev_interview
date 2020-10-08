32abb2c56c927def956f621b0d956b31bb4136e4
Patsheba <44169602+patsheba@users.noreply.github.com>
2020-10-08 14:10:06 +0300

    Create problem_2.js

---
+++ problem_2.js
@@ -0,0 +1,21 @@
+function flatten(flattenArray) {
+  const arrayStack = [...flattenArray];
+  //define empty array to store output
+  const response = [];
+  //while loop to flatten the array no matter the depth
+  while(arrayStack.length) {
+    // pop value from the arrayStack
+    const nextValue = arrayStack.pop();
+    if(Array.isArray(nextValue)) {
+      // push back array items, won't modify the original input
+      arrayStack.push(...nextValue);
+    } else {
+      response.push(nextValue);
+    }
+  }
+  // reverse the order to restore flattenArray order
+  return response.reverse();
+}
+const testArray = [1,2,[3,4,[5,6],7,[8,9]]];
+const flattenedArray = flatten(testArray);
+console.log(flattenedArray)
